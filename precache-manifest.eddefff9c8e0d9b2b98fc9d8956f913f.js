673626c885aa076552715a670ff3dc8205803868
mohamed salamat <moahmed.salamat@gmail.com>
2020-07-19 07:12:41 +0100

    app build

---
+++ precache-manifest.eddefff9c8e0d9b2b98fc9d8956f913f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd973077b88ee0ae72615e146888f7bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23547ec04ee7fac7a374",
+    "url": "/static/css/main.ac2c8904.chunk.css"
+  },
+  {
+    "revision": "3d6dc69b7776b6750c34",
+    "url": "/static/js/2.3796a746.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3796a746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23547ec04ee7fac7a374",
+    "url": "/static/js/main.eee7631e.chunk.js"
+  },
+  {
+    "revision": "7e3d0d3942bca4e19536",
+    "url": "/static/js/runtime-main.ef17e366.js"
+  }
+]);
